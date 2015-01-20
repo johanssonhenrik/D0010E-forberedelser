a5aa4ab1fd4474ebe3a64585adbb68b6c558be5f
johanssonhenrik <johanssonhenrik91@gmail.com>
2015-01-20 14:19:56 +0100

    initial commit
    path done

---
+++ src/forberedelseuppgifter2/Forberedelse2A.java
@@ -0,0 +1,15 @@
+package forberedelseuppgifter2;
+
+public class Forberedelse2A {
+	
+	public static void main(String[] args) {
+		int k = 5;
+		System.out.println(kvadratRekursiv(k));	
+	}
+
+	public static int kvadratRekursiv(int k){
+		
+		
+		return k;
+	}
+}
