1c6955c1fdf7a70100036f285e086a51de79f54b
johanssonhenrik <johanssonhenrik91@gmail.com>
2015-01-21 17:52:10 +0100

    kvadrat #2

--- src/forberedelseuppgifter2/Forberedelse2A.java
+++ src/forberedelseuppgifter2/Forberedelse2A.java
@@ -3,25 +3,19 @@ package forberedelseuppgifter2;
 public class Forberedelse2A {
 	
 	public static void main(String[] args) {
-		int k = 4;
+		int k = 2;
 		System.out.println("The square root of "+ k + " is " + kvadratRekursiv(k));
 	}
-
-	//(4+1) = kvadrat(5) + 2*5 + 1
+//					16	   +  8	 + 1 = 25
+//5^2 =[(4+1) = kvadrat(4) + 2*4 + 1]
 	
 	public static int kvadratRekursiv(int k){
-		int x = 0;
-		k = k + x;
 		
-		if(k == 0){
+		if(k==0){
 			return k;
 		}else{
-			x = k - x + 1;
 			System.out.println(k);
-			kvadratRekursiv(k-1);
+			return kvadratRekursiv(k-1) + k + k + 1;
 		}
-		return k;
 	}
 }
-
-1 4 9 16 25
