ea39a590424fd4e2a710a86eb21ef171063167b4
johanssonhenrik <johanssonhenrik91@gmail.com>
2015-01-22 14:45:50 +0100

    B,D,E - DONE

--- src/forberedelseuppgifter2/Forberedelse2B.java
+++ src/forberedelseuppgifter2/Forberedelse2B.java
@@ -3,18 +3,13 @@ package forberedelseuppgifter2;
 public class Forberedelse2B {
 
 	public static void main(String[] args) {
-
-		siffersumma(32);
+		int n = 19453;
+		System.out.println("The number sum of "+n+" is = "+siffersumma(n));
 	}
-	
 	public static int siffersumma(int n){
 
-		if(n==0){
-			return 0;
-		}
-		
-		return n + siffersumma(n-1);
-
+		if(n==0){return 0;}
+		System.out.println(n%10);
+		return n % 10 + siffersumma(n/10);
 	}
-
-}
+}//Henrik Johansson ehioja-0 2015-January-22
--- src/forberedelseuppgifter2/Forberedelse2E.java
+++ src/forberedelseuppgifter2/Forberedelse2E.java
@@ -3,9 +3,18 @@ package forberedelseuppgifter2;
 public class Forberedelse2E {
 
 	public static void main(String[] args) {
-
-		
-		
+		System.out.println(change(9458));
 	}
-
-}
+	public static String change(int n){
+		
+		int tens = 0;
+		int fives = 0;
+		int ones = 0;
+		
+		tens = tens + n/10;
+		fives = (n%10)/5;
+		ones = (n%5)/1;
+		
+		return "The amount of "+n+" can be split into "+tens+" ten(s), "+fives+" five(s), "+ones+" one(s)";
+	}
+}//Henrik Johansson ehioja-0 2015-January-22
